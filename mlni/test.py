922466c55ff5adfbae38ab2678288cd105fcfde0
anbai106 <anbai106@hotmail.com>
2022-12-28 15:08:00 -0500

    code for MLP2

--- mlni/test.py
+++
@@ -1,6 +0,0 @@
-from adml_regression_mlp import regression_roi
-
-feature_tsv="/cbica/home/wenju/Reproducibile_paper/BrainAge/output/training_tsv/train_muse.tsv"
-output_dir = "/home/hao/test"
-cv_repetition=5
-regression_roi(feature_tsv, output_dir, cv_repetition)
